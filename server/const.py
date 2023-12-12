b2c8230a9ad4196780879775e2a3743202ac8190
bilibob86 <121398999+bilibob86@users.noreply.github.com>
2023-12-12 14:04:26 +0000

    Creation of class Account structure and creation of file const.py with main and curently used data

---
+++ server/const.py
@@ -0,0 +1,11 @@
+#API KEYS
+
+MW_API_KEY = 'c2b0b0f0-8b7b-4e4e-8e4e-4e4e4e4e4e4e'
+DPL_API_kEY = 'c2b0b0f0-8b7b-4e4e-8e4e-4e4e4e4e4e4e'
+
+# DB CONFIG
+
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PASS = ''
+DB_NAME = 'word_quest'

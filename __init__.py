210b8d2d7748591792bab8cb1a09272c31765251
bilibob86 <dev.hpktz@gmail.com>
2024-02-07 11:44:14 +0100

    Add specific and custom error pages

--- __init__.py
+++ __init__.py
@@ -102,5 +102,18 @@ def before_request():
             session['user_data']['route'] = current_route
             session['user_data']['time'] = str(datetime.datetime.now())
 
+# Errors handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('errors/403.html'), 403
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)

59c9678983bffff43fa877deaca9d13bc40a4a39
Abelouuu <haller68.abel@gmail.com>
2024-03-12 10:28:54 +0100

    debug

--- static/styles/games/memory/main.js
+++ static/styles/games/memory/main.js
@@ -33,12 +33,12 @@ async function getCard() {
                 this.innerText = checked['result']['innerHTML']
                 this.classList.add('box_open');
                 document.querySelectorAll('.item').forEach(element => {
-                    element.style.pointerEvents = 'auto'
+                    if (element.classList[1] != '.box_open') {
+                        element.style.pointerEvents = 'auto'
+                    }
                 });
                 if (document.querySelectorAll('.box_open').length == 2) {
-                    document.querySelectorAll('.item').forEach(element => {
                         element.style.pointerEvents = 'none'
-                    });
                     setTimeout(() => {
                         document.querySelectorAll('.item').forEach(element => {
                             if (element.classList[1] == 'box_open') {

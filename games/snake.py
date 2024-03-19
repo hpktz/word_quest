b2027dccfa5f703322329fb7729019ecda91ef48
Abelouuu <haller68.abel@gmail.com>
2024-03-19 09:46:53 +0100

    snake

--- games/snake.py
+++ games/snake.py
@@ -28,6 +28,7 @@ import datetime as datetime
 import math as math
 import uuid as uuid
 import json
+import time as time
 from functools import wraps
 
 import logging
@@ -469,6 +470,7 @@ def getCard(session_id):
     game = snake.from_json(session["game"])
     result = game.newWord()
     session["game"] = game.to_json()
+    time.sleep(1/1000)
     return result
 
 @snake_bp.route('/dashboard/games/snake/<string:session_id>/check_coo', methods=['POST'])
